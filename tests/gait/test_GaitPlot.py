6c81109141ba061c72a768cf39e0f94fc1ba4dfe
Kentrosaurus <kent@Macintosh-6.local>
2012-09-04 18:59:43 -0700

    KAS:  added one more cadence file to test_GaitPlot (un-comment one at a time to see differnet gait plot

--- tests/gait/test_GaitPlot.py
+++ tests/gait/test_GaitPlot.py
@@ -10,8 +10,9 @@ from cadence.plot.gait.GaitPlot import GaitPlot
 gp = GaitPlot(3, 8, 8)
 
 #___________________________________________________________________________________________________ loadData test
-if gp.loadGait("GaitGenerator/PH-50_F-50_H-50.cadence"):
-#if gp.loadGait("GaitGenerator/PH-0_F-50_H-50.cadence"):
+#if gp.loadGait("GaitGenerator/PH-50_F-50_H-50.cadence"):
+if gp.loadGait("GaitGenerator/PH-0_F-50_H-50.cadence"):
+#if gp.loadGait("GaitGenerator/PH-20_F-70_H-70.cadence"):
     print 'SUCCESS: GaitPlot.loadData()'
 else:
     print 'FAILED: GaitPlot.loadData()'
